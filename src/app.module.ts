ebb62f883ed960edf55fe131b5608aeeefb71ec8
Esmyrna <esmyrnaoliveira13@gmail.com>
2024-10-05 23:58:30 -0300

    mergiando branch client

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ClimatologyModule } from './climatology/climatology.module';
+import { AiService } from './ai/ai-service.service';
 
-@Module({ imports: [ClimatologyModule] })
+@Module({ imports: [ClimatologyModule, AiService] })
 export class AppModule {}
