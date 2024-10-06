49f7132e436fca5dcb24a996eb727b1a539544f4
JoaoVNicacio <joaonicks99@gmail.com>
2024-10-06 17:03:29 -0300

    chore & refac: criado dockerfile e ajustado retorno da ia

--- src/ai/ai.controller.ts
+++ src/ai/ai.controller.ts
@@ -1,13 +1,24 @@
 import { Controller, Post, Body } from '@nestjs/common';
-import { AiService } from 'src/ai/ai-service.service';
+import { AiService } from 'src/ai/ai.service';
 
 @Controller('openai')
 export class AIController {
   constructor(private readonly openAIService: AiService) {}
 
   @Post('generate')
-  async generatePrompt(@Body() climateinfo: object, @Body() crops: string[]) {
-    const response = await this.openAIService.generateChatCompletion(climateinfo, crops);
-    return { response };
+  async generatePrompt(
+    @Body('climateinfo') climateinfo: object,
+    @Body('crops') crops: string[],
+  ): Promise<object> {
+    const response = (
+      await this.openAIService.generateChatCompletion(climateinfo, crops)
+    )
+      .replaceAll(`\``, '')
+      .replaceAll(`\n`, '')
+      .replaceAll(`\r`, '')
+      .replace(`json`, '')
+      .trim();
+
+    return JSON.parse(response);
   }
 }
